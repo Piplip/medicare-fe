5c1348de1ae697d5bb3d6f1f439272630cd969c9
Piplip <93427122+Piplip@users.noreply.github.com>
2024-10-09 11:18:33 +0700

    adding styles

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,10 +1,13 @@
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import '../styles/top-nav-style.css'
+import {Link} from "react-router-dom";
 
 export default function TopNav(){
     return (
         <nav id={'top-nav'}>
-            <img className={'logo'} src={'https://cdn.worldvectorlogo.com/logos/chocopie.svg'} alt={'logo'} />
+            <Link to={'/'} className={'nav-logo'}>
+                Medicare<span style={{color: 'orangered'}}>Plus</span>
+            </Link>
             <div id={'nav-cmd'}>
                 <p>Help</p>
                 <NotificationsIcon fontSize={'large'}/>
