9de0ae8f318bb17828e712ad819b42635741e7ec
Piplip <93427122+Piplip@users.noreply.github.com>
2024-10-26 21:30:01 +0700

    adjust UIs

--- src/template/StaffTemplate.jsx
+++ src/template/StaffTemplate.jsx
@@ -5,21 +5,22 @@ import SideNav from "../components/SideNav.jsx";
 import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
-import ScienceIcon from '@mui/icons-material/Science';
 import AnalyticsIcon from '@mui/icons-material/Analytics';
 import SettingsIcon from '@mui/icons-material/Settings';
 import ReceiptIcon from '@mui/icons-material/Receipt';
 import '../styles/side-nav-style.css'
 import '../styles/staff-template-style.css'
+import UnauthenticatedModal from "../components/UnauthenticatedModal.jsx";
 
 export default function StaffTemplate(props){
+    const location = useLocation()
+
     const physicianNavData = {
         "title": "Physician",
         "pages": [
             {"name": "Dashboard", "path": "dashboard", "icon": <DashboardIcon />},
-            {"name": "Patient Management", "path": "patient-view", "icon": <ManageAccountsIcon />},
+            {"name": "Statistics", "path": "statistic", "icon": <AnalyticsIcon />},
             {"name": "Appointment Scheduling", "path": "appointment-scheduling", "icon": <MeetingRoomIcon />},
-            {"name": "Labs & Tests", "path": "labs-tests", "icon": <ScienceIcon />},
         ]
     }
     const adminNavData = {
@@ -32,20 +33,24 @@ export default function StaffTemplate(props){
         ]
     }
 
-    const location = useLocation();
     useEffect(() => {
         window.scrollTo(0, 0);
     }, [location]);
 
     return (
         <div className={'page-template'}>
-            <TopNav/>
-            <section className={'page-content-wrapper'}>
-                <SideNav data={location.pathname.includes('/admin') ? adminNavData : physicianNavData}/>
+            <TopNav homeLink={location.pathname.includes("/admin") ?
+                "/admin/users" : "/physician/dashboard"
+            }/>
+            <section className={'page-content-wrapper'} style={{flexGrow: 1}}>
+                {!location.pathname.includes("/login") && !location.pathname.includes("/signup") &&
+                    <SideNav data={location.pathname.includes('/admin') ? adminNavData : physicianNavData}/>
+                }
                 <div className={'page-template-outlet-wrapper'}>
                     <Outlet context={[props.language, props.setLanguage]}/>
                 </div>
             </section>
+            <UnauthenticatedModal />
         </div>
     )
 }
