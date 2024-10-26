3c2a75b441ac48f984976b7ba7029a5950d44327
Piplip <93427122+Piplip@users.noreply.github.com>
2024-10-26 21:26:01 +0700

    adding check for unauthenticated staff

---
+++ src/components/UnauthenticatedModal.jsx
@@ -0,0 +1,34 @@
+import {Modal, ModalDialog, Stack} from "@mui/joy";
+import {Button, Typography} from "@mui/material";
+import {useEffect, useState} from "react";
+import {useNavigate} from "react-router";
+import {getCookie} from "./Utilities.jsx";
+
+export default function UnauthenticatedModal(){
+    const [open, setOpen] = useState(false)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const staffID = getCookie('STAFF-ID')
+        if(staffID === null && !location.pathname.includes("/login")){
+            setOpen(true)
+        }
+    }, []);
+
+    return (
+        <Modal open={open}>
+            <ModalDialog>
+                <Typography variant={'h4'} color={'red'}>WARNING: ACCESS DENIED</Typography>
+                <Typography variant={'h6'}>
+                    You haven't logged in!<br/>Please log in with your staff account
+                </Typography>
+                <Stack>
+                    <Button variant={'contained'} color={'primary'} onClick={() => {
+                        setOpen(false)
+                        navigate('/staff/login')
+                    }}>LOG IN</Button>
+                </Stack>
+            </ModalDialog>
+        </Modal>
+    )
+}
